1b71efa32503476aa1a282c150c4fbcbc58e07a5
anish7031 <anishshinde7031@gmail.com>
2026-02-03 19:22:47 +0530

    intial code

---
+++ Terraform-root/main.tf
@@ -0,0 +1,27 @@
+module "node_app" {
+  source         = "./Terraform-Node-Project-Cloud"
+  vpc_cidr       = "10.0.0.0/16"
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets= ["10.0.11.0/24", "10.0.12.0/24"]
+  docker_image   = "519848832066.dkr.ecr.us-east-1.amazonaws.com/node-poc:latest"
+}
+
+resource "aws_ecs_service" "service" {
+  name            = "node-service"
+  cluster         = module.node_app.ecs_cluster_id
+  task_definition = module.node_app.task_definition_arn
+  desired_count   = 1
+
+  network_configuration {
+    security_groups = [module.node_app.ecs_sg_id]
+    subnets         = module.node_app.private_subnets
+  }
+
+  load_balancer {
+    target_group_arn = module.node_app.lb_target_group_arn
+    container_name   = "node"
+    container_port   = 3000
+  }
+}
+
+
